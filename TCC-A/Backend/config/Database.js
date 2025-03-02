cc2f2b3b327c3d288754a7b27d95aeb40a80ad9f
Rahmadi Priambudi Riadi <73084651+rahmadi-riadi@users.noreply.github.com>
2025-03-02 22:02:05 +0700

    Delete TCC-A/Backend directory
    hapus

--- TCC-A/Backend/config/Database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize ('hrd','root','',(
-    host :'localhost',
-    dialect: 'mysql'
-))
-
-export default db;
-    
--- TCC-A/Backend/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import cors from "cors";
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.listen(5000, ()=>console.log('sedang berjalan...'));
